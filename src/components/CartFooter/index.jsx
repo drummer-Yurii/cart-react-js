31cc6187883065543b15fac04d387e297ca3e5a8
drummer-Yurii <104525685+drummer-Yurii@users.noreply.github.com>
2022-10-23 17:17:11 +0300

    first commit

---
+++ src/components/CartFooter/index.jsx
@@ -0,0 +1,16 @@
+import "./style.scss";
+import formatPrice from "./../../utils/priceFormatter";
+
+const CartFooter = ({total}) => {
+    const { count, price } = total;
+    return (
+        <footer className="cart-footer">
+            <div className="cart-footer__count">{count} ед.</div>
+            <div className="cart-footer__price">
+                {formatPrice(price)} руб.
+            </div>
+        </footer>
+    );
+}
+
+export default CartFooter;
